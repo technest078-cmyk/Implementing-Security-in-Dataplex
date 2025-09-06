65dbf17de88f5d5d2e13bc08a9a4e0c5873127ab
Tech Nest <technest078@gmail.com>
2025-09-06 21:15:33 +0530

    Update TechNest1157.sh

--- TechNest1157.sh
+++ TechNest1157.sh
@@ -8,13 +8,13 @@ export PROJECT_ID=$(gcloud config get-value project)
 
 gcloud services enable dataplex.googleapis.com datacatalog.googleapis.com
 
-gcloud dataplex lakes create customer-info-lake --location=$REGION --display-name="Techcps Info Lake"
+gcloud dataplex lakes create customer-info-lake --location=$REGION --display-name="TechNest Info Lake"
 
-gcloud dataplex zones create customer-raw-zone --location=$REGION --display-name="Techcps Raw Zone" --lake=customer-info-lake --type=RAW --resource-location-type=SINGLE_REGION
+gcloud dataplex zones create customer-raw-zone --location=$REGION --display-name="TechNest Raw Zone" --lake=customer-info-lake --type=RAW --resource-location-type=SINGLE_REGION
 
 gcloud dataplex assets create customer-online-sessions \
   --location=$REGION \
-  --display-name="Techcps Online Sessions" \
+  --display-name="Technest Online Sessions" \
   --lake=customer-info-lake \
   --zone=customer-raw-zone \
   --resource-type=STORAGE_BUCKET \
